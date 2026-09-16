4b619bcb40c29486394e05ce45d7d5cf42579cc7
Weltkind666 <bogdan.malyx@gmail.com>
2026-09-16 17:48:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* Weltkind PWA Service Worker — v59 */
-const SW_VER = 59;
-const CACHE = 'Weltkind-v59';
+/* Weltkind PWA Service Worker — v60 */
+const SW_VER = 60;
+const CACHE = 'Weltkind-v60';
 const SUB_KEY = 'weltkind-sub-data';
 const SUB_CACHE = 'Weltkind-user-data';
 const ASSETS = [
